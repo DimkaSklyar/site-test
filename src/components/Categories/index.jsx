922ddb42a2245c639e13fa9375c124370da670a9
Dmitry <30568388+DimkaSklyar@users.noreply.github.com>
2020-10-07 23:19:06 +0300

    v.0.2

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -3,19 +3,29 @@ import React, { useState } from "react";
 function Categories({ items }) {
   const [selectItem, setSelectItem] = useState(null);
 
+  const selectItemCategory = (index) => {
+    setSelectItem(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        {items.map((name, index) => (
-          <li
-            className={selectItem === index ? "active" : ""}
-            key={`${name}_${index}`}
-            onClick={() => setSelectItem(index)}
-          >
-            {name}
-          </li>
-        ))}
+        <li
+          className={selectItem === null ? "active" : ""}
+          onClick={() => selectItemCategory(null)}
+        >
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={selectItem === index ? "active" : ""}
+              key={`${name}_${index}`}
+              onClick={() => selectItemCategory(index)}
+            >
+              {name}
+            </li>
+          ))}
       </ul>
     </div>
   );
